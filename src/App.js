2289d25f88567700880886693384284e7413cd67
Punith Nandiraj <punithnandiraj@gmail.com>
2020-10-25 20:36:50 +0530

    Added Subtotal and Checkout Components

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./Components/Home/Home";
 import Header from "./Components/Header/Header";
+import Checkout from "./Components/Checkout/Checkout";
 function App() {
 return (
   <div className="app">
     <Router>
       <Header/>
       <Switch>
+      <Route path="/checkout">
+          <Checkout/>
+        </Route>
         <Route path="/">
           <Home/>
         </Route>
+
       </Switch>
     </Router>
   </div>
